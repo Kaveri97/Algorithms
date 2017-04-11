a8df89ce47e59b1a6e0074e789ee8190c3a7e043
Kaveri97 <kaverimukundan2013@gmail.com>
2017-04-11 17:10:49 +0530

    FCTRL:Factorial

---
+++ factFn.cpp
@@ -0,0 +1,17 @@
+ #include<bits/stdc++.h>
+using namespace std;
+//FCTRL:Factorial 
+int main(){
+int T;
+cin>>T;
+for(int i=0;i<T;i++){
+long int n;
+cin>>n;
+long int count = 0;
+while(n>0){
+count += n/5;
+n /= 5;
+}
+cout<<count<<endl;
+}
+}
