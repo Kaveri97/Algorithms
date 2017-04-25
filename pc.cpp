85d9ccd380f13d54f18dc7fc59da33cade89ef40
Kaveri97 <kaverimukundan2013@gmail.com>
2017-04-25 21:19:07 +0530

    Packaging Cupcakes

---
+++ pc.cpp
@@ -0,0 +1,16 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+     
+    int main()
+    {
+    int t;
+    long long int n,ans;
+    cin >> t;
+    while(t--)
+    {
+    cin >> n;
+    ans=(n/2)+1;
+    cout << ans << endl;
+    }
+    return 0;
+    } 
