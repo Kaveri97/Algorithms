f27b03a8acd904027232cf2939feb1bbfb04bb29
Kaveri97 <kaverimukundan2013@gmail.com>
2017-05-12 23:11:22 +0530

    Pattern Syntax Checker

---
+++ Solution67.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class Solution67
+{
+   public static void main(String[] args){
+      Scanner in = new Scanner(System.in);
+      int testCases = Integer.parseInt(in.nextLine());
+      while(testCases>0){
+            try
+              {
+             String pattern = in.nextLine();
+              Pattern.compile(pattern);
+                  System.out.println("Valid");
+              }
+
+         catch (PatternSyntaxException e)
+              {
+              System.out.println("Invalid");
+              }
+          testCases--;
+   }
+   }
+}
+
