01d29db8048d5d84ce47ec86b656f75a1371bb2e
Kaveri97 <kaverimukundan2013@gmail.com>
2017-08-23 20:22:18 +0530

    Reverse String

---
+++ RSL.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string reverseString(string s) {
+        long long int n = s.length();
+        for(int i=0;i<n/2;i++){
+            char t = s[i];
+            s[i] = s[n-1-i];
+            s[n-1-i] = t;
+        }
+        return s;
+    }
+};
